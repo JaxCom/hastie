94a84fed788249200d971c22cc1ab9973eb15f6a
Jeff Brown <jbrown@candojax.org>
2014-05-09 04:44:15 -0400

    removing config from version control

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sourcedir = "/var/www/{{SOURCE DIRECTORY}}"; //Directory for Jekyll source files
-$webroot = "/var/www/{{SOURCE DIRECTORY}}/_site"; //Directory for Jekyll to build into
-$siteurl = "{{YOUR MAIN SITE URL}}"; //Your website's main URL
-$backendurl = "{{URL FOR HYDE}}"; //The web URL for Hyde
-$sitename = "{{YOUR SITE NAME}}"; //Your website's name
-$hydetitle = "Hyde"; //Set a custom title for the navigation bar
-$postsdir = "/var/www/{{SOURCE DIRECTORY}}/_posts"; //Directory to store blog posts in
